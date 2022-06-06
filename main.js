c6ac245bb492fc6056d2344b37a087dad5544846
Hye <hyekyunghan@live.com>
2022-06-06 19:33:11 -0300

    Melhorando o código js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function tocaSom(listaDeSons) {
-	document.querySelector(listaDeSons).play();
+function tocaSom(seletorAudio) {
+	const elemento = document.querySelector(seletorAudio);
+
+	if(elemento != null && elemento.localName === 'audio') {
+		elemento.play();
+	} else {
+		console.log('Elemento não encontrado ou seletor inválido');
+	}
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -13,5 +19,12 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 	tecla.onclick = function() {
 		tocaSom(idAudio);
 	}
-
+	tecla.onkeydown = function(evento) {
+		if (evento.code === "Enter" || evento.code === "Space") {
+			tecla.classList.add('ativa');
+		}
+	}
+	tecla.onkeyup = function() {
+		tecla.classList.remove('ativa');
+	}
 }
